79554f450431186b5a5a0507d9734e79243b5c71
Rookie <1442044077@qq.com>
2020-05-01 22:39:05 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: root
+  Date: 2020/5/1
+  Time: 20:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Test</title>
+  </head>
+  <body>
+  <h1 style="color: orange">Hello Idea</h1>
+  </body>
+</html>
---
+++ Test1/src/Hello.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println();
+        Map map=new HashMap();
+
+        List list=new ArrayList();
+        for (Object o : list) {
+            System.out.println();
+        }
+
+    }
+
+
+}
+
+class a{
+    private  String t;
+
+
+}
